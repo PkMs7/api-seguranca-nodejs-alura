4ca9326392493ff6ecbecb0fe51c11c6ae3ca868
PkMs7 <patrickmarques2009@hotmail.com>
2024-05-21 17:14:45 -0300

    Criação da rota de autenticação de login com npm install jsonwebtoken

---
+++ api/services/authService.js
@@ -0,0 +1,42 @@
+const database = require('../models')
+const { compare } = require('bcryptjs')
+const { sign } = require('jsonwebtoken')
+const jsonSecret = require('../config/jsonSecret')
+
+const tempoExpiraToken = 86400
+
+class AuthService {
+
+    async login(dto) {
+
+        const usuario = await database.usuarios.findOne({
+            attributes: ['id', 'email', 'senha'], 
+            where: {
+                email: dto.email
+            }
+        })
+
+        if (!usuario) {
+            throw new Error('Usuário não cadastrado!')
+        }
+
+        const senhasIguais = await compare(dto.senha, usuario.senha)
+
+        if (!senhasIguais) {
+            throw new Error('Usuário ou senha inválido')
+        }
+
+        const accessToken = sign({
+            id: usuario.id,
+            email: usuario.email
+        }, jsonSecret.secret, {
+            expiresIn: tempoExpiraToken
+        })
+
+        return { accessToken }
+
+    }
+
+}
+
+module.exports = AuthService
