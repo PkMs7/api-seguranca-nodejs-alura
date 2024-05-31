560c81e6fbaf3d833a5c44f8097e7a02e2b1e084
PkMs7 <patrickmarques2009@hotmail.com>
2024-05-31 11:38:58 -0300

    Configuração da model de usuários para relacionamento entre usuários e perfis de usuários

--- api/models/usuarios.js
+++ api/models/usuarios.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      usuarios.belongsToMany(models.perfisUsuarios, {
+        through: models.usuarios_perfis,
+        as: 'usuarios_perfis',
+        foreignKey: 'usuario_id'
+      })
     }
   }
   usuarios.init({
